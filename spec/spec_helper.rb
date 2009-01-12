a67ab31b0f166a46e461f5e577a279097cf860bd
Anthony Williams <anthony@ninecraft.com>
2009-01-12 23:30:18 +0000

    Add BeLineTuple spec helper (with specs of it's own).
    Replaces the four separate helper classes added in [cddcd74].

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-$TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 require 'iniparse'
 require File.join(File.dirname(__FILE__), 'spec_fixtures')
@@ -32,163 +31,111 @@ module IniParse
         end
       end
 
-      # Asserts that a Tuple returned by Line#parse is a valid tuple for
-      # creating a Section.
-      class BeSectionTuple
-        def initialize(key) # + args
-          @key = key
+      # Used as a base for creating tuple matchers.
+      class BeLineTuple
+        def initialize(type, value_keys = [], *expected)
+          @expected_type = type
+          @value_keys    = value_keys
+
+          if expected.nil?
+            @expected_opts   = {}
+            @expected_values = []
+          else
+            @expected_opts   = expected.pop
+            @expected_values = expected
+          end
         end
 
-        def matches?(target)
-          @target = target
+        def matches?(tuple)
+          @tuple = tuple
 
-          if @target.nil? || (! @target.kind_of?(Array))
-            @nil_value = true
-            false
-          elsif @target[0] != :section
-            @wrong_type = true
-            false
-          elsif @target[1] != @key
-            @wrong_key = true
-            false
-          else
+          @failure_message = catch(:fail) do
+            tuple?
+            correct_type?
+            correct_values?
+            correct_opts?
             true
           end
+
+          @failure_message == true
         end
 
         def failure_message
-          if @nil_value
-            "expected section tuple but got #{@target.class}"
-          elsif @wrong_type
-            "expected section tuple but was :#{@target[0]}"
-          elsif @wrong_key
-            "expected section tuple with key `#{@key}` but was `#{@target[1]}`"
-          end
+          "expected #{@expected_type} tuple #{@failure_message}"
         end
 
         def negative_failure_message
-          "expected #{@expected} to not be section tuple"
+          "expected #{@tuple.inspect} to not be #{@expected_type} tuple"
         end
 
         def description
-          "be_section_tuple #{@target}"
-        end
-      end
-
-      # Asserts that a Tuple returned by Line#parse is a valid tuple for
-      # creating an Option.
-      class BeOptionTuple
-        def initialize(key, value) # + args
-          @key   = key
-          @value = value
+          "be_#{@expected_type}_tuple #{@tuple}"
         end
 
-        def matches?(target)
-          @target = target
+        #######
+        private
+        #######
 
-          if @target.nil? || (! @target.kind_of?(Array))
-            @nil_value = true
-            false
-          elsif @target[0] != :option
-            @wrong_type = true
-            false
-          elsif @target[1] != @key
-            @wrong_key = true
-            false
-          elsif @target[2] != @value
-            @wrong_value = true
-            false
-          else
-            true
+        # Matchers.
+
+        def tuple?
+          throw :fail, "but was #{@tuple.class}" unless @tuple.kind_of?(Array)
+        end
+
+        def correct_type?
+          throw :fail, "but was #{type} tuple" unless type == @expected_type
+        end
+
+        def correct_values?
+          # Ensure the received tuple has the correct number of values.
+          if values.length != @value_keys.length
+            throw :fail, "with %d values but had %d values" % [
+              @value_keys.length, values.length
+            ]
+          end
+
+          # Make sure the values match.
+          @value_keys.each_with_index do |key, i|
+            if @expected_values[i] != :any && values[i] != @expected_values[i]
+              throw :fail, 'with %s value of "%s" but was "%s"' % [
+                key, values[i], @expected_values[i]
+              ]
+            end
           end
         end
 
-        def failure_message
-          if @nil_value
-            "expected option tuple but got #{@target.class}"
-          elsif @wrong_type
-            "expected option tuple but was :#{@target[0]}"
-          elsif @wrong_key
-            "expected option tuple with key `#{@key}` but was `#{@target[1]}`"
-          elsif @wrong_value
-            "expected option tuple with value `#{@value}` but was `#{@target[2]}`"
+        def correct_opts?
+          if(! @expected_opts.nil?)
+            if (! @expected_opts.empty?) && opts.empty?
+              throw :fail, 'with options but there were none'
+            end
+
+            @expected_opts.each do |key, value|
+              unless opts.has_key?(key)
+                throw :fail, 'with option "%s", but key was not present' % key
+              end
+
+              unless opts[key] == value
+                throw :fail, 'with option "%s" => "%s" but was "%s"' % [
+                  key, value, opts[key]
+                ]
+              end
+            end
           end
         end
 
-        def negative_failure_message
-          "expected #{@expected} to not be option tuple"
+        # Tuple values, etc.
+
+        def type
+          @type ||= @tuple.first
         end
 
-        def description
-          "be_option_tuple #{@target}"
-        end
-      end
-
-      # Asserts that a Tuple returned by Line#parse is a valid tuple for
-      # creating a Blank.
-      class BeBlankTuple
-        def matches?(target)
-          @target = target
-
-          if @target.nil? || (! @target.kind_of?(Array))
-            @nil_value = true
-            false
-          elsif @target[0] != :blank
-            @wrong_type = true
-            false
-          else
-            true
-          end
+        def values
+          @values ||= @tuple.length < 3 ? [] : @tuple[1..-2]
         end
 
-        def failure_message
-          if @nil_value
-            "expected blank tuple but got #{@target.class}"
-          elsif @wrong_type
-            "expected blank tuple but was :#{@target[0]}"
-          end
-        end
-
-        def negative_failure_message
-          "expected #{@expected} to not be blank tuple"
-        end
-
-        def description
-          "be_blank_tuple #{@target}"
-        end
-      end
-
-      # Asserts that a Tuple returned by Line#parse is a valid tuple for
-      # creating a Comment.
-      class BeCommentTuple
-        def matches?(target)
-          @target = target
-
-          if @target.nil? || (! @target.kind_of?(Array))
-            @nil_value = true
-            false
-          elsif @target[0] != :comment
-            @wrong_type = true
-            false
-          else
-            true
-          end
-        end
-
-        def failure_message
-          if @nil_value
-            "expected comment tuple but got #{@target.class}"
-          elsif @wrong_type
-            "expected comment tuple but was :#{@target[0]}"
-          end
-        end
-
-        def negative_failure_message
-          "expected #{@expected} to not be comment tuple"
-        end
-
-        def description
-          "be_comment_tuple #{@target}"
+        def opts
+          @opts ||= @tuple.last
         end
       end
 
@@ -196,20 +143,20 @@ module IniParse
         BeKindOf.new(expected)
       end
 
-      def be_section_tuple(key = nil)
-        BeSectionTuple.new(key)
+      def be_section_tuple(key = :any, opts = {})
+        BeLineTuple.new(:section, [:key], key, opts)
       end
 
-      def be_option_tuple(key = nil, value = nil)
-        BeOptionTuple.new(key, value)
+      def be_option_tuple(key = :any, value = :any, opts = {})
+        BeLineTuple.new(:option, [:key, :value], key, value, opts)
       end
 
       def be_blank_tuple
-        BeBlankTuple.new
+        BeLineTuple.new(:blank)
       end
 
-      def be_comment_tuple
-        BeCommentTuple.new
+      def be_comment_tuple(comment = :any, opts = {})
+        BeLineTuple.new(:comment, [:comment], comment, opts)
       end
 
       def fixture(fix)
