ba729ceb5cfd66708152417666247f8289eb7519
Anthony Williams <anthony@ninecraft.com>
2009-01-13 00:58:21 +0000

    Further simplify Parser#parse and move line sanitation to the parser.
    Moving the line sanitation to the parser has allowed for higher-level
    testing of line parsing, such that all specs for #parse on the various
    Line classes have been removed in favour of new ones which parse a
    line all the way through - from removing indents and comments, through
    to returning the correct tuple.
    
    The old spec/parser_spec.rb specs have also been cleaned up a bit and
    moved to the new spec/parser subdirectory.

--- lib/iniparse/parser.rb
+++ lib/iniparse/parser.rb
@@ -20,26 +20,72 @@ module IniParse
     # IniParse::Document
     #
     def parse
-      generator = IniParse::Generator.new
+      IniParse::Generator.gen do |generator|
+        @source.split("\n", -1).each do |line|
+          generator.send(*Parser.parse_line(line))
+        end
+      end
+    end
 
-      @source.split("\n", -1).each_with_index do |line, i|
-        sanitized, opts = IniParse::Lines::Line.sanitize_line(line)
+    class << self
+      # Takes a raw line from an INI document, striping out any inline
+      # comment, and indent, then returns the appropriate tuple so that the
+      # Generator instance can add the line to the Document.
+      #
+      # ==== Raises
+      # IniParse::ParseError: If the line could not be parsed.
+      #
+      def parse_line(line)
+        sanitized, opts = strip_indent(*strip_comment(line.dup, {}))
 
         parsed = @@parse_types.reduce(nil) do |memo, type|
           memo ||= type.parse(sanitized, opts)
         end
 
-        if parsed
-          generator.send(*parsed)
-        else
+        if parsed.nil?
           raise IniParse::ParseError, <<-EOS.compress_lines
             A line of your INI document could not be parsed to a LineType:
-            '#{line}' (line #{i+1}).
+            '#{line}'.
           EOS
         end
+
+        parsed
       end
 
-      generator.document
+      #######
+      private
+      #######
+
+      # Strips in inline comment from a line (or value), removes trailing
+      # whitespace and sets the comment options as applicable.
+      def strip_comment(line, opts)
+        if m = /^(.*?)(?:\s+(;|\#)\s*(.*))$/.match(line) ||
+           m = /(^)(?:(;|\#)\s*(.*))$/.match(line) # Comment lines.
+          opts[:comment] = m[3].rstrip
+          opts[:comment_sep] = m[2]
+          # Remove the line content (since an option value may contain a
+          # semi-colon) _then_ get the index of the comment separator.
+          opts[:comment_offset] =
+            line[(m[1].length..-1)].index(m[2]) + m[1].length
+
+          line = m[1]
+        else
+          line.rstrip!
+        end
+
+        [line, opts]
+      end
+
+      # Removes any leading whitespace from a line, and adds it to the options
+      # hash.
+      def strip_indent(line, opts)
+        if m = /^(\s+).*$/.match(line)
+          line.lstrip!
+          opts[:indent] = m[1]
+        end
+
+        [line, opts]
+      end
     end
   end
 end
--- spec/parser/document_parsing_spec.rb
+++ spec/parser/document_parsing_spec.rb
@@ -1,16 +1,12 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/../spec_helper'
 
-describe 'IniParse::Parser#parse' do
-  def parse(source)
-    IniParse::Parser.new(source).parse
-  end
+# Tests parsing of multiple lines, in context, using #parse.
 
-  it 'should return an IniParse::Document' do
-    parse('').should be_kind_of(IniParse::Document)
-  end
-
-  describe 'with :comment_before_section fixture' do
-    before(:all) { @doc = parse(fixture(:comment_before_section)) }
+describe 'Parsing a document' do
+  describe 'when a comment preceeds a single section and option' do
+    before(:all) do
+      @doc = IniParse::Parser.new(fixture(:comment_before_section)).parse
+    end
 
     it 'should have a comment as the first line' do
       @doc.lines.to_a.first.should be_kind_of(IniParse::Lines::Comment)
@@ -25,27 +21,30 @@ describe 'IniParse::Parser#parse' do
     end
   end
 
-  it 'should allow comments to preceed the first section' do
-    lambda { parse(fixture(:comment_before_section)) }.should_not raise_error
-  end
-
   it 'should allow blank lines to preceed the first section' do
-    lambda { @doc = parse(fixture(:blank_before_section)) }.should_not raise_error
+    lambda {
+      @doc = IniParse::Parser.new(fixture(:blank_before_section)).parse
+    }.should_not raise_error
+
     @doc.lines.to_a.first.should be_kind_of(IniParse::Lines::Blank)
   end
 
   it 'should allow a blank line to belong to a section' do
-    lambda { @doc = parse(fixture(:blank_in_section)) }.should_not raise_error
+    lambda {
+      @doc = IniParse::Parser.new(fixture(:blank_in_section)).parse
+    }.should_not raise_error
+
     @doc['first_section'].lines.to_a.first.should be_kind_of(IniParse::Lines::Blank)
   end
 
   it 'should raise an error if an option preceeds the first section' do
-    lambda { parse(fixture(:option_before_section)) }.should \
-      raise_error(IniParse::NoSectionError)
+    lambda {
+      IniParse::Parser.new(fixture(:option_before_section)).parse
+    }.should raise_error(IniParse::NoSectionError)
   end
 
   it 'should raise ParseError if a line could not be parsed' do
-    lambda { parse(fixture(:invalid_line)) }.should \
+    lambda { IniParse::Parser.new(fixture(:invalid_line)).parse }.should \
       raise_error(IniParse::ParseError)
   end
 end
