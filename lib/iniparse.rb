ab632e03454952865770b1b884cd3e99569d9f46
Anthony Williams <hi@antw.io>
2015-08-04 16:25:06 +0100

    Version bump to 1.4.1

--- lib/iniparse.rb
+++ lib/iniparse.rb
@@ -7,7 +7,7 @@ require File.join(dir, 'lines')
 require File.join(dir, 'parser')
 
 module IniParse
-  VERSION = '1.4.0'
+  VERSION = '1.4.1'
 
   # A base class for IniParse errors.
   class IniParseError < StandardError; end
